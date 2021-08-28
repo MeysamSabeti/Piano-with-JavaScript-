e21c8cf0ef09e1cfc1117e49c7f61cb610351b24
meysam sabeti <meysam_sabeti8@yahoo.com>
2021-08-28 17:02:43 +0430

    set audio file for each button

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 const note = document.querySelector('.nowplaying');
-
+const keys = document.querySelectorAll('.key')
+const hints = document.querySelectorAll('.hints')
 
 window.addEventListener('keydown' , function(e) {
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     if( ! key) return;
 
     const keyNote = key.getAttribute('data-note');
 
-    note.innerHTML = keyNote
 
+    key.classList.add('playing')
+    note.innerHTML = keyNote
+    audio.currentTime=0;
+    audio.play()
 })
+
+
+keys.forEach(key=>key.addEventListener('transitionend',removeTransition))
+
+hints.forEach(function(elm,index) {
+    // elm.style = `transition-delay : ${index *50}ms`
+    elm.setAttribute('style',`transition-delay : ${index *50}ms`)
+})
+
+
+function removeTransition() {
+    this.classList.remove('playing')
+}
